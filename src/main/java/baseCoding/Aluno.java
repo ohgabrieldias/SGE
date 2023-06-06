45d9b1ef0874fbc88a5a246cdbd4800a5491320b
gabrieldias <gabrielrosadias@gmail.com>
2023-06-06 18:51:57 -0300

    refatorando classo Aluno

--- src/main/java/baseCoding/Aluno.java
+++ src/main/java/baseCoding/Aluno.java
@@ -1,24 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package baseCoding;
+
+import sge.MySQLConnector.MySQLConnector;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Aluno extends Pessoa {
     private long matricula;
-    
-    public Aluno(String nome, int cpf, String end, long matricula) {
-        super(nome,cpf,end);
+    MySQLConnector connector = new MySQLConnector();
+
+    public Aluno(String nome, String cpf, String end, long matricula) {
+        super(nome, cpf, end);
         this.matricula = matricula;
     }
-    
+
     // Getters and setters for the 'matricula' attribute
-    
+
     public long getMatricula() {
         return matricula;
     }
-    
+
     public void setMatricula(long matricula) {
         this.matricula = matricula;
+    }
+
+    // Métodos getters e setters para os atributos nome, cpf e end
+
+    public String getNome() {
+        return super.getNome();
+    }
+
+
+    public String getCpf() {
+        return super.getCpf();
+    }
+
+ 
+
+    public String getEnd() {
+        return super.getEndereco();
+    }
+
+
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno("João", "123456789", "Rua A", 123456789);
+        System.out.println("Nome: " + aluno.getNome());
+        System.out.println("CPF: " + aluno.getCpf());
+        System.out.println("Endereço: " + aluno.getEnd());
+        System.out.println("Matrícula: " + aluno.getMatricula());
     }
 }
