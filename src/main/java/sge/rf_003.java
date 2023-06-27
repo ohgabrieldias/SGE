a7a2863a72fb92a922f56494456489f383bdc6df
Gabriel Fredes <gabriel.sfredes@gmail.com>
2023-06-27 19:43:08 -0300

    Tela Cadastro de Funcionário(a)
    - Adicionado a tela de cadastro de funcionário(a)
    - Configurado JCalendar para campo de data de nascimento
    - Adicionado link no menu

--- src/main/java/sge/rf_003.java
+++ src/main/java/sge/rf_003.java
@@ -26,21 +26,221 @@ public class rf_003 extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        logo = new javax.swing.JLabel();
+        titulo = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        nomeLabel = new javax.swing.JLabel();
+        nomeCampo = new javax.swing.JTextField();
+        sobrenomeLabel = new javax.swing.JLabel();
+        cpfLabel = new javax.swing.JLabel();
+        cpfCampo = new javax.swing.JTextField();
+        cpfLabel1 = new javax.swing.JLabel();
+        btnCadastrar = new javax.swing.JButton();
+        loginPanel = new javax.swing.JPanel();
+        senhaLabel = new javax.swing.JLabel();
+        senhaCampo = new javax.swing.JTextField();
+        gerarSenha = new javax.swing.JButton();
+        dnCampo = new com.toedter.calendar.JDateChooser();
+        sobrenomeCampo = new javax.swing.JTextField();
+
+        setClosable(true);
+        setTitle("Cadastro de Funcionário(a)");
+
+        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/funcionarioLogo.png"))); // NOI18N
+
+        titulo.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        titulo.setText("Cadastro de Funcionário(a)");
+
+        nomeLabel.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        nomeLabel.setText("Nome");
+
+        nomeCampo.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        nomeCampo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                nomeCampoActionPerformed(evt);
+            }
+        });
+
+        sobrenomeLabel.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        sobrenomeLabel.setText("Sobrenome");
+
+        cpfLabel.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        cpfLabel.setText("CPF");
+
+        cpfCampo.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        cpfCampo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cpfCampoActionPerformed(evt);
+            }
+        });
+
+        cpfLabel1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        cpfLabel1.setText("Data de nascimento");
+
+        btnCadastrar.setText("Cadastrar");
+
+        loginPanel.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(153, 153, 153), 1, true));
+
+        senhaLabel.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        senhaLabel.setText("Senha");
+
+        senhaCampo.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        senhaCampo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                senhaCampoActionPerformed(evt);
+            }
+        });
+
+        gerarSenha.setText("Gerar Senha");
+        gerarSenha.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                gerarSenhaActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout loginPanelLayout = new javax.swing.GroupLayout(loginPanel);
+        loginPanel.setLayout(loginPanelLayout);
+        loginPanelLayout.setHorizontalGroup(
+            loginPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(loginPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(loginPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(senhaLabel)
+                    .addGroup(loginPanelLayout.createSequentialGroup()
+                        .addComponent(senhaCampo, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(gerarSenha, javax.swing.GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE)))
+                .addGap(12, 12, 12))
+        );
+        loginPanelLayout.setVerticalGroup(
+            loginPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(loginPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(senhaLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(loginPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(senhaCampo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(gerarSenha))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        dnCampo.setDateFormatString("d'/'MM'/'yyyy");
+        dnCampo.setMaxSelectableDate(new java.util.Date(253370779289000L));
+
+        sobrenomeCampo.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        sobrenomeCampo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sobrenomeCampoActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(nomeCampo, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(cpfLabel)
+                            .addComponent(nomeLabel))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(sobrenomeLabel)
+                            .addComponent(cpfLabel1)
+                            .addComponent(sobrenomeCampo, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(btnCadastrar)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(cpfCampo, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(dnCampo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(logo)
+                                .addGap(29, 29, 29)
+                                .addComponent(titulo))
+                            .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(loginPanel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(0, 25, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(logo))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(32, 32, 32)
+                        .addComponent(titulo)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(nomeLabel)
+                    .addComponent(sobrenomeLabel))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(nomeCampo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(sobrenomeCampo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cpfLabel)
+                    .addComponent(cpfLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(cpfCampo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(dnCampo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(loginPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
+                .addComponent(btnCadastrar, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(30, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void nomeCampoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nomeCampoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_nomeCampoActionPerformed
+
+    private void cpfCampoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cpfCampoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cpfCampoActionPerformed
+
+    private void senhaCampoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_senhaCampoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_senhaCampoActionPerformed
+
+    private void gerarSenhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_gerarSenhaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_gerarSenhaActionPerformed
+
+    private void sobrenomeCampoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sobrenomeCampoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_sobrenomeCampoActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCadastrar;
+    private javax.swing.JTextField cpfCampo;
+    private javax.swing.JLabel cpfLabel;
+    private javax.swing.JLabel cpfLabel1;
+    private com.toedter.calendar.JDateChooser dnCampo;
+    private javax.swing.JButton gerarSenha;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JPanel loginPanel;
+    private javax.swing.JLabel logo;
+    private javax.swing.JTextField nomeCampo;
+    private javax.swing.JLabel nomeLabel;
+    private javax.swing.JTextField senhaCampo;
+    private javax.swing.JLabel senhaLabel;
+    private javax.swing.JTextField sobrenomeCampo;
+    private javax.swing.JLabel sobrenomeLabel;
+    private javax.swing.JLabel titulo;
     // End of variables declaration//GEN-END:variables
 }
