22bed2e9fd2fda20bcb481e483560d149827e7c5
gabrieldias <gabrielrosadias@gmail.com>
2023-06-27 23:20:46 -0300

    refact: refatoran metodo dicionarAluno

--- src/main/java/sge/rf_001.java
+++ src/main/java/sge/rf_001.java
@@ -7,11 +7,7 @@ package sge;
 import  baseCoding.Aluno;
 import util.Formater;
 import javax.swing.JOptionPane;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import util.Validator;
 
 /**
  *
@@ -21,87 +17,6 @@ public class rf_001 extends javax.swing.JInternalFrame {
 
     public rf_001() {
         initComponents();
-    }
-
-    private boolean validarNome(String nome) {
-        // Verifique se o nome não está vazio
-        if (nome.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Nome inválido");
-            return false;
-        }
-    
-        // Adicione outras validações específicas para o nome, se necessário
-    
-        return true;
-    }
-    
-    private boolean validarSobrenome(String sobrenome) {
-        // Verifique se o sobrenome não está vazio
-        if (sobrenome.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Sobrenome inválido");
-            return false;
-        }
-    
-        // Adicione outras validações específicas para o sobrenome, se necessário
-    
-        return true;
-    }
-    
-    private boolean validarDataNascimento(String dataNascimento) {
-        // Verifique se a data de nascimento não está vazia
-        if (dataNascimento.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Data de nascimento inválida");
-            return false;
-        }
-    
-        // Adicione outras validações específicas para a data de nascimento, se necessário
-    
-        return true;
-    }
-    
-    private boolean validarCPF(String cpf) {
-        // Remova caracteres especiais do CPF (pontos e traço)
-        cpf = cpf.replaceAll("[^0-9]", "");
-    
-        // Verifique se o CPF possui 11 dígitos
-        if (cpf.length() != 11) {
-            JOptionPane.showMessageDialog(this, "CPF inválido");
-            return false;
-        }
-    
-        // Verifique se todos os dígitos do CPF são iguais (CPF inválido)
-        if (cpf.matches("(\\d)\\1{10}")) {
-            JOptionPane.showMessageDialog(this, "CPF inválido");
-            return false;
-        }
-    
-        // Calcule o primeiro dígito verificador
-        int soma = 0;
-        for (int i = 0; i < 9; i++) {
-            soma += (cpf.charAt(i) - '0') * (10 - i);
-        }
-        int digito1 = 11 - (soma % 11);
-        if (digito1 > 9) {
-            digito1 = 0;
-        }
-    
-        // Calcule o segundo dígito verificador
-        soma = 0;
-        for (int i = 0; i < 10; i++) {
-            soma += (cpf.charAt(i) - '0') * (11 - i);
-        }
-        int digito2 = 11 - (soma % 11);
-        if (digito2 > 9) {
-            digito2 = 0;
-        }
-    
-        // Verifique se os dígitos verificadores calculados são iguais aos dígitos informados
-        if (digito1 != (cpf.charAt(9) - '0') || digito2 != (cpf.charAt(10) - '0')) {
-            JOptionPane.showMessageDialog(this, "CPF inválido");
-            return false;
-        }
-    
-        return true;
     }
     
     /**
@@ -206,6 +121,8 @@ public class rf_001 extends javax.swing.JInternalFrame {
         respAviso.setText("Somente se necessário um responsável");
         respAviso.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
+        dataNascimento.setDateFormatString("dd/MM/yyyy");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -245,7 +162,7 @@ public class rf_001 extends javax.swing.JInternalFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(novoAlunoCPFRespLabel)
                             .addComponent(novoAlunoCPFResp, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addComponent(respAviso, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(respAviso, javax.swing.GroupLayout.DEFAULT_SIZE, 325, Short.MAX_VALUE))
                 .addGap(25, 25, 25))
         );
         layout.setVerticalGroup(
@@ -306,37 +223,26 @@ public class rf_001 extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_novoAlunoNomeActionPerformed
 
     private void btnCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarActionPerformed
-        
-    // Obtém a data de nascimento no formato dd/MM/yyyy
-    String dataNascimento = null;
-        try {
-            dataNascimento = Formater.formatarData(novoAlunoDN.getText());
-        } catch (ParseException ex) {
-            Logger.getLogger(rf_001.class.getName()).log(Level.SEVERE, null, ex);
+        String nome = novoAlunoNome.getText();
+        String sobrenome = novoAlunoSobrenome.getText();
+        String dataNasc = "";
+        String cpf = novoAlunoCPF.getText();
+        String cpfResp = novoAlunoCPFResp.getText();
+        String resp = novoAlunoResp.getText();
+        String end = "";
+        dataNasc = Formater.formatarData2(dataNascimento);
+      
+        System.out.print("data = " + dataNasc);
+        if (Validator.validarNome(nome) && Validator.validarSobrenome(sobrenome) && Validator.validarCPF(cpf)) {
+
+        Aluno aluno = new Aluno(nome, sobrenome, dataNasc, cpf, "Av n sei onde", resp,cpfResp, 2110102629);
+        aluno.cadastrarAluno(aluno);
+
+        JOptionPane.showMessageDialog(this, "Cadastro bem sucedido!");
+
+        // Feche o formulário
+        dispose();
         }
-
-    // Verifique se mysqlDateString não está vazia antes de usá-la
-    if (mysqlDateString != null && !mysqlDateString.isEmpty()) {
-        // Faça o que você precisa com mysqlDateString
-        System.out.println("Data de nascimento em formato MySQL: " + mysqlDateString);
-
-        if (validarNome(novoAlunoNome.getText()) && validarSobrenome(novoAlunoSobrenome.getText()) &&
-            validarDataNascimento(mysqlDateString) && validarCPF(novoAlunoCPF.getText())) {
-
-            Aluno aluno = new Aluno(novoAlunoNome.getText(), novoAlunoSobrenome.getText(), dataNascimento, novoAlunoCPF.getText(), "Av n sei onde", novoAlunoResp.getText(), novoAlunoCPFResp.getText(), 2021002);
-            aluno.cadastrarAluno(aluno);
-
-            JOptionPane.showMessageDialog(this, "Cadastro bem sucedido!");
-
-            // Feche o formulário
-            dispose();
-            }
-        
-    } else {
-        // Caso mysqlDateString esteja vazia, mostre uma mensagem de erro ou realize outra ação apropriada
-        System.out.println("Erro ao converter a data de nascimento.");
-    }
-
         
         
     }//GEN-LAST:event_btnCadastrarActionPerformed
