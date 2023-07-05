08cb43c3d52918bebeb6f49b27a15f0b06f5a9ac
Gabriel Fredes <gabriel.sfredes@gmail.com>
2023-07-04 23:20:17 -0300

    Tela adicionada
    - Adicionada tela para movimentação de cursos
    - Adicionado link para abertura da tela ao main

--- src/main/java/sge/main.java
+++ src/main/java/sge/main.java
@@ -49,6 +49,7 @@ public class main extends javax.swing.JFrame {
         rf_005Menu = new javax.swing.JMenuItem();
         menuMovimentar = new javax.swing.JMenu();
         rf_009 = new javax.swing.JMenuItem();
+        jMenuItem1 = new javax.swing.JMenuItem();
         menuFinanceiro = new javax.swing.JMenu();
         menuRelatorios = new javax.swing.JMenu();
         rf_006Menu = new javax.swing.JMenuItem();
@@ -158,6 +159,16 @@ public class main extends javax.swing.JFrame {
         });
         menuMovimentar.add(rf_009);
 
+        jMenuItem1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jMenuItem1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/curso.png"))); // NOI18N
+        jMenuItem1.setText("Cursos");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        menuMovimentar.add(jMenuItem1);
+
         mainMenu.add(menuMovimentar);
 
         menuFinanceiro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/menuFinanceiro.png"))); // NOI18N
@@ -252,6 +263,12 @@ public class main extends javax.swing.JFrame {
         rfMovimentAaluno.setVisible(true);
     }//GEN-LAST:event_rf_009ActionPerformed
 
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        rf_014 rfMovimentaCurso = new rf_014();
+        areaTrabalhoMain.add(rfMovimentaCurso);
+        rfMovimentaCurso.setVisible(true);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -293,6 +310,7 @@ public class main extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JDesktopPane areaTrabalhoMain;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JLabel logoMain;
     private javax.swing.JMenuBar mainMenu;
     private javax.swing.JMenu menuCadastros;
